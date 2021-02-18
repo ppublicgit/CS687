42ff1fd5968d5c07f4a8911c4d0130f8a84544c7
p.public <p.public@protonmail.com>
2021-02-17 21:34:39 -0600

    my sql linked to python and blank ui set up

---
+++ sql_py_helper.py
@@ -0,0 +1,38 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+class SqlConnection:
+    def __init__(self, host="localhost", database="FAKE_AMAZON", user="root", password="mysqluah"):
+        try:
+            self.connection = mysql.connector.connect(host='localhost',
+                                                 database='FAKE_AMAZON',
+                                                 user='root',
+                                                 password='mysqluah')
+            if self.connection.is_connected():
+                db_Info = self.connection.get_server_info()
+                print("Connected to MySQL Server version ", db_Info)
+                self.cursor = self.connection.cursor()
+                self.cursor.execute("select database();")
+                record = self.cursor.fetchone()
+                print("You're connected to database: ", record)
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+
+
+    def __del__(self):
+        if (self.connection.is_connected()):
+            self.cursor.close()
+            self.connection.close()
+            print("MySQL connection is closed")
+        else:
+            print("Connection already closed")
+
+
+    def close(self):
+        if (self.connection.is_connected()):
+            self.cursor.close()
+            self.connection.close()
+            print("MySQL connection is closed")
+        else:
+            print("Connection already closed")
